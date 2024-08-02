18806d7e3878bcdd94d9aa3ebd0687534fae329b
Mironov_Roman <psychokid534@gmail.com>
2024-08-02 21:25:56 +0300

    add UserSpecification

--- src/main/java/ru/fitnes/fitnestreaker/repository/UserSpecification.java
+++ src/main/java/ru/fitnes/fitnestreaker/repository/UserSpecification.java
@@ -10,42 +10,14 @@ import ru.fitnes.fitnestreaker.entity.User;
 @RequiredArgsConstructor
 public class UserSpecification {
 
-//    private final EntityManager entityManager;
-//    public List<User> findAllByCriteria(UserDto userDto) {
-//        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-//        CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
-//        List<Predicate> predicateList = new ArrayList<>();
-//
-//        Root<User> root = criteriaQuery.from(User.class);
-//        if (userDto.getFirstName() != null) {
-//            Predicate firstNamePredicate = criteriaBuilder
-//                    .like(root.get("firstName"),"%" + userDto.getFirstName() + "%" );
-//            predicateList.add(firstNamePredicate);
-//        }if (userDto.getLastName() != null) {
-//            Predicate lastNamePredicate = criteriaBuilder
-//                    .like(root.get("lastName"),"%" + userDto.getLastName() + "%" );
-//            predicateList.add(lastNamePredicate);
-//        }if (userDto.getEmail() != null) {
-//            Predicate emailPredicate = criteriaBuilder
-//                    .like(root.get("email"),"%" + userDto.getEmail() + "%" );
-//            predicateList.add(emailPredicate);
-//        }
-//        criteriaQuery.where(
-//                criteriaBuilder.or(predicateList.toArray(new Predicate[0]))
-//        );
-//        TypedQuery<User> query = entityManager.createQuery(criteriaQuery);
-//        return query.getResultList();
-//        }
-//    }
 public static Specification<User> hasFirstName(String firstName) {
     return (root, query, criteriaBuilder) -> {
         if (firstName == null || firstName.isEmpty()) {
-            return criteriaBuilder.conjunction(); // всегда истинно
+            return criteriaBuilder.conjunction();
         }
         return criteriaBuilder.like(root.get("firstName"), "%" + firstName + "%");
     };
 }
-
     public static Specification<User> hasLastName(String lastName) {
         return (root, query, criteriaBuilder) -> {
             if (lastName == null || lastName.isEmpty()) {
@@ -54,7 +26,6 @@ public static Specification<User> hasFirstName(String firstName) {
             return criteriaBuilder.like(root.get("lastName"), "%" + lastName + "%");
         };
     }
-
     public static Specification<User> hasEmail(String email) {
         return (root, query, criteriaBuilder) -> {
             if (email == null || email.isEmpty()) {
